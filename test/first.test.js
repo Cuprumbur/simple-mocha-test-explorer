a5191111efb4cefb773e089164a07b1ecbc1d1cc
Ochir Darmaev <ochirdarmaev@gmail.com>
2019-11-20 01:51:30 +0300

    config mocha side bar

---
+++ test/first.test.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const chai_1 = require("chai");
+it('test', () => {
+    const a = 6;
+    const b = 6;
+    const s = a + b;
+    chai_1.expect(12).to.eql(s);
+});
